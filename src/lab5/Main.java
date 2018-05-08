804d32afa1f985cf6e67edb5551515c5db3223a1
Arangeen <arangeen@mail.sfsu.edu>
2018-05-07 19:55:51 -0700

    test passed

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -5,7 +5,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.HashMap;
+
+import java.util.StringTokenizer;
+
+
 public class Main {
+
+  public static HashMap<String,Integer> res = new HashMap<String, Integer>();
 
   public static void main(String[] args) {
     try {
@@ -14,9 +21,29 @@ public class Main {
       String readLine = "";
       while ((readLine = b.readLine()) != null) {
         System.out.println(readLine);
+        parse(readLine);
       }
+      System.out.println(res);
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
+
+  public static void parse(String a)
+  {
+    StringTokenizer line = new StringTokenizer(a);
+    while(line.hasMoreTokens())
+    {
+      String word = line.nextToken();
+
+      if(res.containsKey(word))
+      {
+        int t = res.get(word)+ 1;
+        res.put(word,t);
+      }else{
+        res.put(word, 1);
+      }
+    }
+  }
+
 }
